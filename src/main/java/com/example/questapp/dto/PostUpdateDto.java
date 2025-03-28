e1f69afdd9bac6f913fcbbb75500e3548b5722eb
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2025-03-28 16:35:44 +0300

    Post Controller

---
+++ src/main/java/com/example/questapp/dto/PostUpdateDto.java
@@ -0,0 +1,9 @@
+package com.example.questapp.dto;
+
+import lombok.Data;
+
+@Data
+public class PostUpdateDto {
+    String title;
+    String text;
+}
--- src/main/java/com/example/questapp/repository/PostRepository.java
+++ src/main/java/com/example/questapp/repository/PostRepository.java
@@ -3,5 +3,9 @@ package com.example.questapp.repository;
 import com.example.questapp.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByUserId(Long userId);
 }
