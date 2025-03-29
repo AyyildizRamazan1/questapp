3fe73a09a0c40bf80f1df0c8891c3ae044973b36
Ramazan <97359282+AyyildizRamazan1@users.noreply.github.com>
2025-03-29 10:36:06 +0300

    Comment Controller

---
+++ src/main/java/com/example/questapp/controllers/CommentController.java
@@ -0,0 +1,47 @@
+package com.example.questapp.controllers;
+
+import com.example.questapp.dto.CommentCreateDto;
+import com.example.questapp.dto.CommentUpdateDto;
+import com.example.questapp.entities.Comment;
+import com.example.questapp.services.CommentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping
+    public List<Comment> getAllComments(@RequestParam Optional<Long> userId, @RequestParam Optional<Long> postId) {
+        return commentService.getAllCommentsWithParam(userId, postId);
+    }
+
+    @PostMapping
+    public Comment createOneComment(@RequestBody CommentCreateDto request) {
+        return commentService.createOneComment(request);
+    }
+
+    @GetMapping("/{commentId}")
+    public Comment getOneComment(@PathVariable Long commentId) {
+        return commentService.getOneCommentById(commentId);
+    }
+
+    @PutMapping("/{commentId}")
+    public Comment updateOneComment(@PathVariable Long commentId, @RequestBody CommentUpdateDto request) {
+        return commentService.updateOneCommentById(commentId, request);
+    }
+
+    @DeleteMapping("/{commentId}")
+    public void deleteOneComment(@PathVariable Long commentId) {
+        commentService.deleteOneCommentById(commentId);
+    }
+
+}
